a1468bd62f6abc3abf7f8026231f135202244950
llyama <4733017+llyama@users.noreply.github.com>
2025-07-04 15:00:52 +0100

    Added overloads to shortest_path_length()

--- stubs/networkx/networkx/algorithms/shortest_paths/generic.pyi
+++ stubs/networkx/networkx/algorithms/shortest_paths/generic.pyi
@@ -1,6 +1,7 @@
 from _typeshed import Incomplete
-from collections.abc import Callable, Generator
-from typing import overload
+from collections.abc import Callable, Generator, Iterator
+from typing import Any, Literal, overload
+from typing_extensions import TypeAlias
 
 from networkx.classes.graph import Graph, _Node
 from networkx.utils.backends import _dispatchable
@@ -14,6 +15,9 @@ __all__ = [
     "average_shortest_path_length",
     "has_path",
 ]
+
+ShortestPathMethod: TypeAlias = Literal["dijkstra", "bellman-ford"]
+EdgeWeightFunc: TypeAlias = Callable[[_Node, _Node, dict[str, Any]], float]
 
 @_dispatchable
 def has_path(G: Graph[_Node], source: _Node, target: _Node) -> bool: ...
@@ -41,14 +45,34 @@ def shortest_path(
     weight: str | Callable[..., Incomplete] | None = None,
     method: str | None = "dijkstra",
 ) -> dict[_Node, list[_Node]]: ...
-@_dispatchable
+@overload
+def shortest_path_length(
+    G: Graph[_Node], source: _Node, target: _Node, weight: None = None, method: ShortestPathMethod = "dijkstra"
+) -> int: ...
+@overload
 def shortest_path_length(
     G: Graph[_Node],
-    source: _Node | None = None,
-    target: _Node | None = None,
-    weight: str | Callable[..., Incomplete] | None = None,
-    method: str | None = "dijkstra",
-): ...
+    source: _Node,
+    target: _Node,
+    weight: str | EdgeWeightFunc[_Node] = ...,
+    method: ShortestPathMethod = "dijkstra",
+) -> float: ...
+@overload
+def shortest_path_length(
+    G: Graph[_Node],
+    source: _Node,
+    target: None = None,
+    weight: str | EdgeWeightFunc[_Node] | None = None,
+    method: ShortestPathMethod = "dijkstra",
+) -> dict[_Node, int | float]: ...
+@overload
+def shortest_path_length(
+    G: Graph[_Node],
+    source: None = None,
+    target: None | _Node = None,
+    weight: str | EdgeWeightFunc[_Node] | None = None,
+    method: ShortestPathMethod = "dijkstra",
+) -> dict[_Node, int | float] | Iterator[tuple[_Node, dict[_Node, int | float]]]: ...
 @_dispatchable
 def average_shortest_path_length(
     G: Graph[_Node], weight: str | Callable[..., Incomplete] | None = None, method: str | None = None
